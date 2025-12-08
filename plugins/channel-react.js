412d2d2a00941d8077cd7c949cd5bb4efa3204f0
asvfskadxxc-png <asvfskadxxc@gmail.com>
2025-12-08 06:58:32 -0800

    Update channel-react.js

--- plugins/channel-react.js
+++ plugins/channel-react.js
@@ -45,7 +45,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const channelMeta = await conn.newsletterMetadata("invite", channelId);
         await conn.newsletterReactMessage(channelMeta.id, messageId, emoji);
 
-        return reply(`╭━━━〔 *LOSER-MD* 〕━━━┈⊷
+        return reply(`╭━━━〔 *FAIZAN-F-MD* 〕━━━┈⊷
 ┃▸ *Success!* Reaction sent
 ┃▸ *Channel:* ${channelMeta.name}
 ┃▸ *Reaction:* ${emoji}
