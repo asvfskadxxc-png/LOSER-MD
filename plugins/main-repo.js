cadc29559a2185ec44945cf953b91b4aa99be878
asvfskadxxc-png <asvfskadxxc@gmail.com>
2025-12-08 07:20:02 -0800

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -58,8 +58,8 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '0029Vb6u4UZ1dAw78zbIiL1h@newsletter',
-                    newsletterName: 'LOSER-MD',
+                    newsletterJid: '120363422488713927@newsletter',
+                    newsletterName: 'FAIZAN-F-MD',
                     serverMessageId: 143
                 }
             }
